b569f674b73e4e82e0e26bba6e265b5d4d56f4ed
antonisimelio <antonisimelio@localhost.localdomain>
2019-10-14 11:40:08 +0200

    First Draft for tdkGen10500

---
+++ cmds/tdkGen10500sim.cmd
@@ -0,0 +1,13 @@
+#require streamdevice, 2.7.1
+#require tdkgen10500, nsenaud
+require stream,2.8.8
+require tdkGen10500, 1.0.1
+
+
+drvAsynIPPortConfigure("CoilsPS-01", "127.0.0.1:9999")
+drvAsynIPPortConfigure("CoilsPS-02", "127.0.0.1:9999")
+drvAsynIPPortConfigure("CoilsPS-03", "127.0.0.1:9999")
+#pwd
+#dbLoadRecords("./cmds/test.db")
+#dbLoadRecords("./db/tdkGen10500.db")
+dbLoadRecords("tdkGen10500.db")
---
+++ tdkGen10500-loc/tdkGen10500App/Db/tdkGen10500.db.d
@@ -0,0 +1,1 @@
+tdkGen10500App/Db/tdkGen10500.db: tdkGen10500App/Db/tdkGen10500.template
